779d2c395c60b10fcebf2788719b746a871ffee6
Luna Solar de la Fuente <alfredesdef@gmail.com>
2026-06-24 18:57:27 -0400

    Added per-channel onmsg_trigger feature

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import json
+from uuid import uuid4
 import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
 import random
 
-debug_mode: bool = False
+debug_mode: bool = True
 
 def debug(*msg: str):
     if debug_mode:
@@ -57,6 +58,41 @@ async def on_message(message: discord.Message):
     
     if message.content.startswith('$'):
         await msg_command(message)
+        return
+    
+    if str(message.channel.guild.id) in data.keys():
+        if str(message.channel.id) in data[str(message.channel.guild.id)].keys():
+            if 'onmsg-trigger' in data[str(message.channel.guild.id)][str(message.channel.id)].keys():
+                if 'exclude_roles' in data[str(message.channel.guild.id)][str(message.channel.id)]['onmsg-trigger'].keys():
+                    for roleid in data[str(message.channel.guild.id)][str(message.channel.id)]['onmsg-trigger']['exclude_roles']:
+                        if message.guild.get_role(roleid) in message.author.roles:
+                            debug(f'Ignoring because of global role exception for role {roleid}')
+                if 'actions' in data[str(message.channel.guild.id)][str(message.channel.id)]['onmsg-trigger'].keys():
+                    for triggerid in data[str(message.channel.guild.id)][str(message.channel.id)]['onmsg-trigger']['actions']:
+                        act = data[str(message.channel.guild.id)][str(message.channel.id)]['onmsg-trigger']['actions'][triggerid]
+                        action = act['action']
+                        action_specific = act['action_specific']
+                        
+                        match action:
+                            case 'ban':
+                                try:
+                                    await message.author.ban(
+                                        delete_message_seconds=10,
+                                        reason=action_specific
+                                    )
+                                except discord.errors.Forbidden:
+                                    await message.reply("The action 'ban' has failed due to insufficient permissions")
+                            case 'kick':
+                                try:
+                                    await message.author.kick(
+                                        reason=action_specific
+                                    )
+                                except discord.errors.Forbidden:
+                                    await message.reply("The action 'kick' has failed due to insufficient permissions")
+                            case 'sendmsg':
+                                await message.channel.send(action_specific)
+                            case 'addrole':
+                                await message.author.add_roles(message.channel.guild.get_role(action_specific))
 
 #bot = commands.Bot('ª', intents=intents)
 @client.event
@@ -92,15 +128,20 @@ async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
         debug(f'Guild {message.channel.guild.id} does not have data')
 
 async def msg_command(message: discord.Message):
+    if not (message.channel.permissions_for(message.guild.me).send_messages):
+        return
+    
     msg_text = message.content.removeprefix('$').split(' ')
     mt0 = msg_text[0]
+    msg_channel = message.channel
     if mt0 == 'hello':
         await message.reply(f'Hello, <@{message.author.id}>!')
     elif mt0 == 'hug':
         await message.add_reaction('🫂')
     elif mt0 == 'add_reply_command':
         if not message.author.guild_permissions.manage_guild:
-            await message.channel.send('You need permission to manage guild in order to use this command!')
+            await message.reply('You need permission to manage guild in order to use this command!')
+            return
         message_id_to_apply = msg_text[1]
         debug(f'applying to {message_id_to_apply}')
         emoji_to_apply = msg_text[2]
@@ -148,6 +189,181 @@ async def msg_command(message: discord.Message):
             file.write(json.dumps(data[str(message.channel.guild.id)]))
             file.close()
         await message.reply('Done!')
+    elif mt0 == 'add_channel_onmsg_trigger':
+        if not message.author.guild_permissions.manage_guild:
+            await message.reply('You need permission to manage guild in order to use this command!')
+            return
+        action = msg_text[1] if len(msg_text) > 1 else None
+        action_specific = ' '.join(msg_text[2:]) if len(msg_text) > 2 else None
+        trigger_id = str(uuid4())
+        
+        match action:
+            case 'sendmsg':
+                if not action_specific:
+                    await message.reply('The sendmsg action requires the message content to send to be specified.')
+                    return
+            case 'addrole':
+                if not action_specific:
+                    await message.reply('The role to add must be specifed.')
+            case _:
+                if not action in ['ban', 'kick']:
+                    await message.reply('Invalid action.')
+        
+        debug(f'Data: {data}\nKeys: {data.keys()}')
+        if not str(msg_channel.guild.id) in data.keys():
+            debug('Guild ' + str(msg_channel.guild.id) + ' has not yet been initialised; initialising')
+            data[str(msg_channel.guild.id)] = {
+                msg_channel.id: {
+                    'onmsg-trigger': {
+                        'actions': {
+                            trigger_id: {
+                                'action': action,
+                                'action_specific': action_specific,
+                                'exclude_roles': [],
+                                'exclude_users': [],
+                            }
+                        }
+                    }
+                }
+            }
+        elif not str(msg_channel.id) in data[str(msg_channel.guild.id)].keys():
+            debug('Channel ' + str(msg_channel.id) + ' has not yet been initialised; initialising')
+            data[str(msg_channel.guild.id)][str(msg_channel.id)] = {
+                'onmsg-trigger': {
+                    'actions': {
+                        trigger_id: {
+                            'action': action,
+                            'action_specific': action_specific,
+                            'exclude_roles': [],
+                            'exclude_users': [],
+                        }
+                    }
+                }
+            }
+        elif not ('onmsg-trigger') in data[str(msg_channel.guild.id)][str(msg_channel.id)].keys():
+            debug('Channel did not previously have any onmsg-trigger values')
+            data[str(msg_channel.guild.id)][str(msg_channel.id)]['onmsg-trigger'] = {
+                'actions': {
+                    trigger_id: {
+                        'action': action,
+                        'action_specific': action_specific,
+                        'exclude_roles': [],
+                        'exclude_users': [],
+                    }
+                }
+            }
+        elif not ('actions') in data[str(msg_channel.guild.id)][str(msg_channel.id)]['onmsg-trigger'].keys():
+            debug('Channel did not previously have a list of actions for onmsg-trigger')
+            data[str(msg_channel.guild.id)][str(msg_channel.id)]['onmsg-trigger']['actions'] = {
+                trigger_id: {
+                    'action': action,
+                    'action_specific': action_specific,
+                    'exclude_roles': [],
+                    'exclude_users': [],
+                }
+            }
+        elif not str(trigger_id) in data[str(msg_channel.guild.id)][str(msg_channel.id)]['onmsg-trigger']['actions'].keys():
+            debug(f'Adding trigger of id {trigger_id} to channel {msg_channel.id} of server {msg_channel.guild.id}')
+            data[str(msg_channel.guild.id)][str(msg_channel.id)]['onmsg-trigger']['actions'][trigger_id] = {
+                'action': action,
+                'action_specific': action_specific,
+                'exclude_roles': [],
+                'exclude_users': [],
+            }
+        else:
+            await message.reply(f'The ID ({trigger_id}) we assigned you is already used in this channel for an onmsg-trigger. Please try again.')
+            return
+        await message.reply(f'Successfuly added onmsg trigger with ID {trigger_id}')
+        with open(f'./data/{message.channel.guild.id}.json', 'w') as file:
+            debug(data)
+            file.write(json.dumps(data[str(message.channel.guild.id)]))
+            file.close()
+    elif mt0 == 'edit_channel_onmsg_trigger':
+        if not message.author.guild_permissions.manage_guild:
+            await message.reply('You need permission to manage guild in order to use this command!')
+            return
+        
+        if len(msg_text) < 2:
+            await message.reply('You must specify an onmsg_trigger')
+            return
+        if len(msg_text) < 3:
+            await message.reply('You must specify an action')
+        
+        trigger_id = msg_text[1]
+        action = msg_text[2]
+        
+        if not (
+            (str(msg_channel.guild.id) in data.keys()) and
+            (str(msg_channel.id) in data[str(msg_channel.guild.id)].keys()) and
+            ('onmsg-trigger' in data[str(msg_channel.guild.id)][str(msg_channel.id)].keys()) and
+            ('actions' in data[str(msg_channel.guild.id)][str(msg_channel.id)]['onmsg-trigger'].keys()) and
+            (trigger_id in data[str(msg_channel.guild.id)][str(msg_channel.id)]['onmsg-trigger']['actions'].keys())
+            ):
+            await message.reply('Invalid trigger ID (make sure you are in the correct channel)')
+            return
+        
+        match action:
+            case 'del':
+                data[str(msg_channel.guild.id)][str(msg_channel.id)]['onmsg-trigger']['actions'].pop(trigger_id)
+                await message.reply(f'Successfuly deleted trigger {trigger_id} in channel {msg_channel.id}')
+            case _:
+                await message.reply(f'Invalid action: {action}')
+        
+        with open(f'./data/{message.channel.guild.id}.json', 'w') as file:
+            debug(data)
+            file.write(json.dumps(data[str(message.channel.guild.id)]))
+            file.close()
+    elif mt0 == 'add_onmsg_trigger_channel_role_exception':
+        if not message.author.guild_permissions.manage_roles:
+            await message.reply('You need permission to manage roles in order to use this command!')
+            return
+        
+        if len(msg_text) < 2:
+            await message.reply('ERROR: Must specify role as role ID')
+            return
+        
+        applicable_channel = int(msg_text[2], base=0) if len(msg_text) > 2 else message.channel.id
+        
+        role: discord.Role = message.guild.get_role(int(msg_text[1]))
+        
+        if not role:
+            await message.reply('ERROR: invalid role')
+            return
+        
+        if not str(msg_channel.guild.id) in data.keys():
+            data[str(msg_channel.guild.id)] = {
+                str(msg_channel.id): {
+                    'onmsg-trigger': {
+                        'exclude_roles': [
+                            int(msg_text[1])
+                        ]
+                    }
+                }
+            }
+        elif not str(msg_channel.id) in data[str(msg_channel.guild.id)].keys():
+            data[str(msg_channel.guild.id)][str(msg_channel.id)] = {
+                'onmsg-trigger': {
+                    'exclude_roles': [
+                        int(msg_text[1])
+                    ]
+                }
+            }
+        elif not 'onmsg-trigger' in data[str(msg_channel.guild.id)][str(msg_channel.id)].keys():
+            data[str(msg_channel.guild.id)][str(msg_channel.id)]['onmsg-trigger'] = {
+                'exclude_roles': [
+                    int(msg_text[1])
+                ]
+            }
+        elif not 'exclude_roles' in data[str(msg_channel.guild.id)][str(msg_channel.id)]['onmsg-trigger'].keys():
+            data[str(msg_channel.guild.id)][str(msg_channel.id)]['onmsg-trigger']['exclude_roles'] = [
+                int(msg_text[1])
+            ]
+        elif not msg_text[1] in data[str(msg_channel.guild.id)][str(msg_channel.id)]['onmsg-trigger']['exclude_roles'].keys():
+            data[str(msg_channel.guild.id)][str(msg_channel.id)]['onmsg-trigger']['exclude_roles'].push(int(msg_text[1]))
+        else:
+            await message.reply('Role has already been added to this channel\'s global onmsg-trigger role exclusions')
+            return
+        await message.reply(f'Added role {role.name} (ID: {role.id}) to this channel\'s global onmsg-trigger role exclusions')
     else:
         await message.reply(f'Invalid command: {msg_text[0]}')
 
